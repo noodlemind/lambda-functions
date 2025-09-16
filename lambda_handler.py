daf78f0b6f34ebcb0102ca10c099b3f159897561
Krish <bvkcp1989@gmail.com>
2025-09-16 07:33:42 -0400

    fix: remove unnecessary files

--- lambda_handler.py
+++
@@ -1,8 +0,0 @@
-"""
-AWS Lambda handler entry point.
-This file should be at the root of your deployment package.
-"""
-from lambda_function.handler import lambda_handler
-
-# AWS Lambda will call this function
-__all__ = ['lambda_handler']
